032dd8bd0179661e6a6c25ce5a3cb03e54306a4f
GuvensConsultora <guvensconsultora@gmail.com>
2025-10-16 07:02:23 -0300

    Crear secuencias de impresión en el momento que instalamos el modulo de remitos preimpresos

--- stock_preprinted_delivery_settings/__manifest__.py
+++ stock_preprinted_delivery_settings/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Tu Organización",
     "website": "https://example.com",
     "license": "LGPL-3",
-    "depends": ["stock"],
+    "depends": ["stock","operating_unit","stock_operating_unit"],
     "data": [
         "security/ir.model.access.csv",
         "views/stock_picking_type_views.xml",
@@ -14,6 +14,7 @@
         "views/hello_wizard_views.xml",
         "views/stock_picking_replace_print.xml",
     ],
+    "post_init_hook": "post_init_set_print_sequences_on_types",     # Función que voy a llamar
     "assets": {},
     "installable": True,
     "application": False
